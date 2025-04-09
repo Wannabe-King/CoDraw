7af1ad60a0d4d5fc475e06b78b08763c886e9d1e
Priyanshu Sidar <sidarsunita2@gmail.com>
2025-04-10 00:39:25 +0530

    created basic auth endpoints and ws connection

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,25 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  console.log("url: ", ws.url);
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  if (!token) {
+    return;
+  }
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
   ws.on("error", (e) => {
     console.error;
   });
